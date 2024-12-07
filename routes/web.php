669a05f12e80ab96f03a6b50890438fc856bb8d9
andrnnnn <andrianjack1@gmail.com>
2024-12-08 00:35:43 +0700

    Implement category routes in web.php for creating and deleting categories

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MangaController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -42,7 +43,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/admin/manga', [MangaController::class, 'index'])->name('manga.index');
         Route::get('/admin/manga/create', [MangaController::class, 'createForm'])->name('manga.create.form');
         Route::post('/admin/manga/create', [MangaController::class, 'create'])->name('manga.create');
-        
+
         Route::get('/admin/manga/{manga}/update', [MangaController::class, 'updateForm'])->name('manga.update.form');
         Route::put('/admin/manga/{manga}', [MangaController::class, 'update'])->name('manga.update');
         Route::delete('/admin/manga/{manga}', [MangaController::class, 'destroy'])->name('manga.destroy');
@@ -53,4 +54,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin/orders/pending', [OrderController::class, 'pending'])->name('orders.pending');
     Route::get('/admin/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
     Route::patch('/admin/orders/{order}/status', [OrderController::class, 'updateStatus'])->name('orders.updateStatus');
+
+    // Category Routes
+    Route::post('/categories', [CategoryController::class, 'create'])->name('category.create');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
 });
