e2a9c624907870762c0a1d50ccdc9a2c754053d1
andrnnnn <andrianjack1@gmail.com>
2024-12-10 22:57:22 +0700

    Add user dashboard, manga detail, and cart routes

--- routes/web/user.php
+++ routes/web/user.php
@@ -4,7 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 // User Routes
 Route::middleware(['auth', 'role:user'])->prefix('user')->name('user.')->group(function () {
+    // Dashboard
     Route::get('/dashboard', function () {
         return view('user.dashboard');
     })->name('dashboard');
+
+    // Detail manga
+    Route::get('/manga/{id}', function() {
+        return view('user.manga-detail');
+    })->name('manga.detail');
+
+    // Keranjang
+    Route::get('/cart', function() {
+        return view('user.cart');
+    })->name('cart');
 });
